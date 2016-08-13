a110d168df81afa72fac316587dd8851726da8d4
cvetkov-a <sashobgb@abv.bg>
2016-08-13 22:40:59 +0300

    Register, Login, Posts

---
+++ FitnessHardSoft/FitnessHardSoft/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using FitnessHardSoft.Models;
+
+namespace FitnessHardSoft.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET POSTS
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(3);
+            return View(posts.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Trainers()
+        {
+            ViewBag.Message = "Your trainer page.";
+
+            return View();
+        }
+    }
+}
