576b598ab893629d41340680b04af0c34a9870e0
cvetkov-a <sashobgb@abv.bg>
2016-08-18 23:56:36 +0300

    Upd

--- FitnessHardSoft/FitnessHardSoft/Models/ApplicationUser.cs
+++ FitnessHardSoft/FitnessHardSoft/Models/ApplicationUser.cs
@@ -10,8 +10,11 @@ namespace FitnessHardSoft.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        [StringLength(100)]
-        public string FullName { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
