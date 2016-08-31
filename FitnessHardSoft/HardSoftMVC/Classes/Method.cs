2a8dc3c49bd1c66951fcd1b28bbb36d630f2a863
1b0x <ib0x@abv.bg>
2016-08-31 19:43:13 +0300

    Update!

---
+++ FitnessHardSoft/HardSoftMVC/Classes/Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HardSoftMVC.Classes
+{
+    public class Method
+    {
+        public string TruncateAtWord(string value, int length, string someText = "...")
+        {
+            if (value == null || value.Length < length || value.IndexOf(" ", length) == -1)
+                return value;
+
+            return value.Substring(0, value.IndexOf(" ", length)) + someText;
+        }
+    }
+}
